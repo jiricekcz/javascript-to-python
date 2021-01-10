f8a02c79917a3a6afd4a2eee795c8f3118f22fc1
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-01-10 15:04:45 +0100

    test update

--- src/translator/out.py
+++ src/translator/out.py
@@ -16,8 +16,9 @@ class Array(list):
         x = self.join(',')
         self.__update()
         return x
-    def push(self, element):
-        self.append(element)
+    def push(self, *elements):
+        for element in elements:
+            self.append(element)
         self.__update()
     def concat(self, array):
         for x in array:
@@ -60,12 +61,64 @@ class Array(list):
         sort(0, self.length - 1)
         self.__update()
         return self
+    def splice(self, start, deleteCount):
+        del self[start:start + deleteCount]
+        self.__update()
+    def unshift(self, *elements):
+        for element in elements:
+            self.insert(0, element)
+        self.__update()
+    def indexOf(self, element):
+        self.index(element)
+    def lastIndexOf(self, element):
+        i = len(self) - 1
+        while i > 0:
+            if (element == self[i]): return i
+    def every(self, condition):
+        i = 0
+        while i < len(self) - 1:
+            if (not self.__callElementCallback(condition, self[i], i, self)): return False
+            i+=1
+        return True
+    def some(self, condition):
+        i = 0
+        while i < len(self) - 1:
+            if (self.__callElementCallback(condition, self[i], i, self)): return True
+            i+=1
+        return False
+    def __callElementCallback(self, callback, element, index, array, arg, arg2, arg3):
+        argCount = callback.__code__.co_argcount - len(callback.__defaults__)
+        if (argCount == 0):
+            return callback()
+        if (argCount == 1):
+            return callback(element)
+        if (argCount == 2):
+            return callback(element, index)
+        if (argCount == 3):
+            return callback(element, index, array)
+        if (argCount == 4):
+            return callback(element, index, array, arg)
+        if (argCount == 5):
+            return callback(element, index, array, arg, arg2)
+        if (argCount == 6):
+            return callback(element, index, array, arg, arg2, arg3)
+        
+    def forEach(self, callback):
+        i = 0
+        while i < len(self) - 1:
+            self.__callElementCallback(callback, self[i], i, self)
+            i+=1
+    def filter(self, callback):
+        h = []
+        i = 0
+        while i < len(self) - 1:
+            if (self.__callElementCallback(callback, self[i], i, self)): h.append(self[i])
+            i+=1
+        return self(h)
+    
 
-def clmlgtdokogdutiugtoglmlinxkzoezj(a,b): return a-b
+
+def fxfiiqopdlspnkceggmorgmkurpqgehi(a,b): return a-b
 a=Array((1,2,5,4,8,7,9,6,21,8,4,6,4))
 a[0]
-print(a.sort(clmlgtdokogdutiugtoglmlinxkzoezj))
-
-def h(a):
-    return a
-print(h)
+print(a.sort(fxfiiqopdlspnkceggmorgmkurpqgehi))
